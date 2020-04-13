55751ac3b1e492a95783c1c7235f7799fdb81b7d
Nam Seob Seo <nsclass@gmail.com>
2020-04-13 14:11:19 -0500

    using canvas

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React, {useState} from 'react';
-import './App.css';
-import Renderer from './components/Renderer';
+import React, { useState } from "react"
+import "./App.css"
+import Renderer from "./components/Renderer"
 
 const App = () => {
   const [loading, setLoading] = useState(false)
   const [universe, setUniverse] = useState(null)
+  const [wasmMemory, setWasmMemory] = useState(null)
 
   const loadWasm = async () => {
     try {
       setLoading(true)
-      console.log("loading...")      
-      const wasm = await import('wasm-game-of-life');
-      setUniverse(wasm.Universe.new());
-    } catch(err) {
-      console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`);
+      console.log("loading...")
+      const wasm = await import("wasm-game-of-life")
+      setUniverse(wasm.Universe.new())
+
+      const memory = await import("wasm-game-of-life/wasm_game_of_life_bg")
+      setWasmMemory(memory)
+    } catch (err) {
+      console.error(`Unexpected error in loadWasm. [Message: ${err.message}]`)
     } finally {
       setLoading(false)
     }
-  };
+  }
 
   if (!universe && !loading) {
     loadWasm()
   }
 
-  if (!universe) {
+  if (!universe || !wasmMemory) {
     return (
       <div className="container">
         <h1 className="title">loading...</h1>
       </div>
     )
   }
-  
-  return (
-    <section>
-      <div className="container">
-        <Renderer universe={universe} />
-      </div>      
-    </section>
-  );
+
+  return <Renderer universe={universe} wasmMemory={wasmMemory} />
 }
 
-export default App;
+export default App
--- src/components/Renderer.js
+++ src/components/Renderer.js
@@ -1,18 +1,85 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import useAnimationFrame from "./UseAnimationFrame"
 
-const Renderer = ({universe}) => {
-  const [scene, setScene] = useState("")
+const CELL_SIZE = 5 // px
+const GRID_COLOR = "#CCCCCC"
+const DEAD_COLOR = "#FFFFFF"
+const ALIVE_COLOR = "#000000"
 
-  useAnimationFrame(deltaTime => {
-    const current = universe.render()
+const Renderer = ({ universe, wasmMemory }) => {
+  // const [scene, setScene] = useState("")
+  const [needsDraw, setNeedsDraw] = useState(false)
+  const canvasRef = React.useRef(null)
+
+  const [canvasWidth, setCanvasWidth] = useState((CELL_SIZE + 1) * universe.width() + 1)
+  const [canvasHeight, setCanvasHeight] = useState((CELL_SIZE + 1) * universe.height() + 1)
+
+  const drawGrid = (ctx) => {
+    const width = universe.width()
+    const height = universe.height()
+
+    ctx.beginPath()
+    ctx.strokeStyle = GRID_COLOR
+
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0)
+      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1)
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+      ctx.moveTo(0, j * (CELL_SIZE + 1) + 1)
+      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1)
+    }
+
+    ctx.stroke()
+  }
+
+  const getIndex = (row, column) => {
+    const width = universe.width()
+    return row * width + column
+  }
+
+  const drawCells = (ctx) => {
+    const width = universe.width()
+    const height = universe.height()
+
+    const cellsPtr = universe.cells()
+    const cells = new Uint8Array(wasmMemory.memory.buffer, cellsPtr, width * height)
+
+    ctx.beginPath()
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col)
+
+        ctx.fillStyle = cells[idx] === 0 ? DEAD_COLOR : ALIVE_COLOR
+
+        ctx.fillRect(col * (CELL_SIZE + 1) + 1, row * (CELL_SIZE + 1) + 1, CELL_SIZE, CELL_SIZE)
+      }
+    }
+
+    ctx.stroke()
+  }
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext("2d")
+    drawGrid(ctx)
+    drawCells(ctx)
+    setNeedsDraw(false)
+  })
+
+  useAnimationFrame((deltaTime) => {
     universe.tick()
-    setScene(current)
+    setNeedsDraw(true)
   })
 
   return (
-    <div className="container">
-      <div id="game-of-life-canvas">{scene}</div>
+    <div>
+      <label className="label">test</label>
+      <canvas id="game-of-life-canvas" ref={canvasRef} width={canvasWidth} height={canvasHeight} />
     </div>
   )
 }
