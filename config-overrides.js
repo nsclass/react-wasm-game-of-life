87b182584829b60f114857f13d42ed7898a7dc08
Nam Seob Seo <nsclass@gmail.com>
2021-03-28 19:39:01 -0500

    docs

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = function override(config, env) {
-  const wasmExtensionRegExp = /\.wasm$/;
+  const wasmExtensionRegExp = /\.wasm$/
 
-  config.resolve.extensions.push('.wasm');
+  config.resolve.extensions.push(".wasm")
 
-  config.module.rules.forEach(rule => {
-    (rule.oneOf || []).forEach(oneOf => {
-      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+  config.module.rules.forEach((rule) => {
+    ;(rule.oneOf || []).forEach((oneOf) => {
+      if (oneOf.loader && oneOf.loader.indexOf("file-loader") >= 0) {
         // make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
+        oneOf.exclude.push(wasmExtensionRegExp)
       }
-    });
-  });
+    })
+  })
 
   // add a dedicated loader for WASM
   config.module.rules.push({
     test: wasmExtensionRegExp,
-    include: path.resolve(__dirname, 'src'),
-    use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-  });
+    include: path.resolve(__dirname, "src"),
+    use: [{ loader: require.resolve("wasm-loader"), options: {} }],
+  })
 
-  return config;
-};
+  return config
+}
