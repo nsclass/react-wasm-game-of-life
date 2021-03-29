c315ee373959758d901cc15a5f08e2c7315c051c
Nam Seob Seo <nsclass@gmail.com>
2021-03-28 19:46:54 -0500

    Updates

---
+++ precache-manifest.66e0e5bba018e6e1aaf8078ed9912046.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363e7804cf77cea4002f",
+    "url": "/2c29ca6e9473042bc4b0.module.wasm"
+  },
+  {
+    "revision": "14250d9b506019362e02f3dffefc56cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cf3011abac6f00b63a",
+    "url": "/static/css/main.dbd4dc0d.chunk.css"
+  },
+  {
+    "revision": "363e7804cf77cea4002f",
+    "url": "/static/js/0.77d50486.chunk.js"
+  },
+  {
+    "revision": "e22f5acaaf29859ed164",
+    "url": "/static/js/3.c6aa8c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/3.c6aa8c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87cf3011abac6f00b63a",
+    "url": "/static/js/main.3551e278.chunk.js"
+  },
+  {
+    "revision": "2f6f4cf4515cc742a19f",
+    "url": "/static/js/runtime-main.00451ec2.js"
+  }
+]);
