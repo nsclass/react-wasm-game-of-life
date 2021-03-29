2f873204171f528375bb0118a5ddf7d22a1ff505
Nam Seob Seo <nsclass@gmail.com>
2021-03-28 19:55:58 -0500

    Updates

---
+++ precache-manifest.cd3e2e952f078cbdd690594aab8e6ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ad8890e7c949bb3cfd",
+    "url": "/react-wasm-game-of-life/2c29ca6e9473042bc4b0.module.wasm"
+  },
+  {
+    "revision": "542a19c14810463c818c8e9770e2d7b6",
+    "url": "/react-wasm-game-of-life/index.html"
+  },
+  {
+    "revision": "9a3347c334a89cebe32f",
+    "url": "/react-wasm-game-of-life/static/css/main.dbd4dc0d.chunk.css"
+  },
+  {
+    "revision": "29ad8890e7c949bb3cfd",
+    "url": "/react-wasm-game-of-life/static/js/0.2b339945.chunk.js"
+  },
+  {
+    "revision": "e22f5acaaf29859ed164",
+    "url": "/react-wasm-game-of-life/static/js/3.c6aa8c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-wasm-game-of-life/static/js/3.c6aa8c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3347c334a89cebe32f",
+    "url": "/react-wasm-game-of-life/static/js/main.7274948a.chunk.js"
+  },
+  {
+    "revision": "9c4377ce5c2b8991c569",
+    "url": "/react-wasm-game-of-life/static/js/runtime-main.513b59bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66e0e5bba018e6e1aaf8078ed9912046.js"
+  "/react-wasm-game-of-life/precache-manifest.cd3e2e952f078cbdd690594aab8e6ef5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-wasm-game-of-life/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
